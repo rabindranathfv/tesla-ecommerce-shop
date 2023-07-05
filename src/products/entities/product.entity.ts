352aa164cbd8175daf5155304adb26c8b3e34af7
Rabindranath Ferreira <ferreiralox@gmail.com>
2023-07-05 22:34:56 +0200

    use BeforeInsert for slug creation

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert } from 'typeorm';
 
 @Entity()
 export class Product {
@@ -31,4 +31,12 @@ export class Product {
 
   @Column('text')
   gender: string;
+
+  @BeforeInsert()
+  checkSlug() {
+    if (!this.slug) {
+      this.slug = this.title.toLowerCase().trim();
+    }
+    this.slug = this.slug.replaceAll(' ', '_').replaceAll("'", '');
+  }
 }
