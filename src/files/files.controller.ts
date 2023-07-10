bb9360dc3a01d5eee9002fd9a250d0152e4a1f04
Rabindranath Ferreira <ferreiralox@gmail.com>
2023-07-10 23:50:04 +0200

    using pipes and interceptors for handle files, create resource files for generate all process files and save into specific folder

---
+++ src/files/files.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('/product')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './static/uploads',
+      }),
+    }),
+  )
+  uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        fileIsRequired: true,
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 3 }), // size of files in bytes
+          new FileTypeValidator({ fileType: '.(png|jpeg|jpg|gif)' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    console.log(
+      '🚀 ~ file: files.controller.ts:10 ~ FilesController ~ uploadFile ~ file:',
+      file,
+    );
+    return this.filesService.uploadFile();
+  }
+}
---
+++ src/files/files.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class FilesService {
+  uploadFile() {
+    return `This action returns all files`;
+  }
+}
