afd5ace2a4b7ac3254bc0bf8bf7fc65b38a05fad
Rabindranath Ferreira <ferreiralox@gmail.com>
2023-07-11 00:47:02 +0200

    endpoint for return images save on the filesystem

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,26 +1,36 @@
 import {
   Controller,
   FileTypeValidator,
+  Get,
   MaxFileSizeValidator,
+  Param,
   ParseFilePipe,
   Post,
   UploadedFile,
   UseInterceptors,
+  Res,
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { fileNamer } from './helpers/fileName.helpers';
+import { Response } from 'express';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
+  @Get('product/:imageName')
+  getImageByName(@Res() res: Response, @Param('imageName') imageName: string) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    res.sendFile(path);
+  }
+
   @Post('/product')
   @UseInterceptors(
     FileInterceptor('file', {
       storage: diskStorage({
-        destination: './static/uploads',
+        destination: './static/products',
         filename: fileNamer,
       }),
     }),
@@ -41,6 +51,6 @@ export class FilesController {
       '🚀 ~ file: files.controller.ts:10 ~ FilesController ~ uploadFile ~ file:',
       file,
     );
-    return this.filesService.uploadFile();
+    return file.originalname;
   }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { join } from 'path';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { existsSync } from 'fs';
 
 @Injectable()
 export class FilesService {
-  uploadFile() {
-    return `This action returns all files`;
+  getStaticProductImage(imageName: string) {
+    const path = join(__dirname, '../../static/products', imageName);
+    console.log(
+      '🚀 ~ file: files.service.ts:9 ~ FilesService ~ getStaticProductImage ~ path:',
+      path,
+    );
+
+    if (!existsSync(path)) {
+      throw new BadRequestException(`No product found with image ${imageName}`);
+    }
+
+    return path;
   }
 }
